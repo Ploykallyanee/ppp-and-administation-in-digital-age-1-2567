5ec818045f7f34b793212901eb74a844853f259c
“Ploykallayanee <kalyanibuysiy@gmail.com>
2024-07-17 15:33:30 +0700

    first commit

---
+++ src/app/aecrangking/page.tsx
@@ -0,0 +1,10 @@
+import AECRangking from "@/components/AECRangking";
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <AECRangking />
+    </div>
+  );
+}
